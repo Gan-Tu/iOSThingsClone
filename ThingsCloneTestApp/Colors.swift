a83ae7c6153cb5f3e42e8f7d0242523e05b7dc03
Gan Tu <github@tugan.dev>
2023-07-09 22:37:39 -0400

    Add some checklist items

---
+++ ThingsCloneTestApp/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  ThingsCloneTestApp
+//
+//  Created by Gan Tu on 7/9/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    static let darkgray = Color("darkgray");
+}
--- ThingsCloneTestApp/ContentView.swift
+++ ThingsCloneTestApp/ContentView.swift
@@ -8,14 +8,153 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedItemId : UUID? = nil
+    
+    private var hasExpandedBlock: Bool {
+        selectedItemId != nil
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack(alignment: .leading) {
+                HStack(alignment: .center) {
+                    Image(systemName: "circle.righthalf.filled")
+                        .foregroundColor(.accentColor)
+                    Text("Things to do")
+                        .bold()
+                    Image(systemName: "chevron.right")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                        .bold()
+                }
+                .padding(5)
+                
+                Divider()
+
+                CheckListItem(text: "Find Wallet", selectedItemId: $selectedItemId)
+
+                CheckListItem(text: "Buy Groceries", selectedItemId: $selectedItemId)
+
+                Spacer()
+            }
+            .padding()
+            .foregroundColor(.black)
+            .onTapGesture {
+                withAnimation {
+                    selectedItemId = nil
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct Checkbox: View {
+    @Binding var isComplete: Bool
+    
+    var body: some View {
+        Button(action: {
+            withAnimation {
+                isComplete.toggle()
+            }
+        }, label: {
+            Image(systemName: isComplete ? "checkmark.square.fill" : "square")
+                .imageScale(.large)
+                .foregroundColor(isComplete ? .accentColor : .gray)
+        })
+    }
+}
+
+struct CheckListItem: View {
+    var id = UUID()
+    var text = ""
+
+    @Binding var selectedItemId: UUID?
+
+    @State private var isComplete = false
+    
+    private var isExpanded: Bool {
+        selectedItemId == id
+    }
+    
+    var body: some View {
+        if !isExpanded {
+            Button(action: {
+                withAnimation {
+                    selectedItemId = id
+                }
+            }, label: {
+                HStack(alignment: .top) {
+                    Checkbox(isComplete: $isComplete)
+                    VStack(alignment: .leading) {
+                        Text(text)
+                            .foregroundColor(isComplete ? .gray : .black)
+                            .strikethrough(isComplete)
+                        Spacer()
+                    }
+                    .frame(alignment: .top)
+                    Spacer()
+                }
+                .padding(5)
+                .frame(height: isExpanded ? 200 : 30, alignment: .top)
+            })
+        } else {
+            HStack(alignment: .top) {
+                Checkbox(isComplete: $isComplete)
+                
+                VStack(alignment: .leading) {
+                    Text(text)
+                        .foregroundColor(isComplete ? .gray : .black)
+                    
+                    Text("Notes")
+                        .foregroundColor(.gray)
+                        .padding(.top, 2)
+                        .frame(height: 50, alignment: .top)
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Button(action: {
+                            // todo
+                        }, label: {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.yellow)
+                            Text("Today")
+                                .font(.callout)
+                                .foregroundColor(.black)
+                                .bold()
+                        })
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            // todo
+                        }, label: {
+                            Image(systemName: "tag")
+                                .rotationEffect(.degrees(270))
+                        })
+                        
+                        Button(action: {
+                            // todo
+                        }, label: {
+                            Image(systemName: "list.bullet")
+                        })
+                        
+                        Button(action: {
+                            // todo
+                        }, label: {
+                            Image(systemName: "flag")
+                        })
+                    }
+                    .foregroundColor(.gray)
+                }
+                .frame(alignment: .top)
+                
+                Spacer()
+            }
+            .padding(5)
+            .frame(height: isExpanded ? 200 : 30, alignment: .top)
+        }
+        
     }
 }
 
