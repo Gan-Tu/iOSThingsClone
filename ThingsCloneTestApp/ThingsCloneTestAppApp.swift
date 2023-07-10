c6cd49951e475e6a76efff9149e6f501beed28ba
Gan Tu <github@tugan.dev>
2023-07-09 22:15:19 -0400

    Initial Commit

---
+++ ThingsCloneTestApp/ThingsCloneTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThingsCloneTestAppApp.swift
+//  ThingsCloneTestApp
+//
+//  Created by Gan Tu on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ThingsCloneTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
